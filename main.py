a0e7475a092f1b4df8a694b7dd636e41a7962cb1
Karol Młynek <73079431+KarlStazysta@users.noreply.github.com>
2023-04-28 20:11:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ def main():
     user_columns = int(input('How many columns? '))
     game_of_life_board = Board(user_rows, user_columns)
     game_of_life_board.draw_board()
-    user_action = ''
-    '''while user_action != 'q':'''
     for i in range(100):
-        '''user_action = input('Press enter to add generation or press q to quit:')'''
-        '''if user_action == '':'''
         game_of_life_board.update_board()
         os.system('printf \'\\33c\\e[3J\'')
         game_of_life_board.draw_board()
